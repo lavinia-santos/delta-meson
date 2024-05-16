33cbaf3e64659b3ddacdb96ba6f1c3f3e06810f9
lavinia.santos <laviniagtsantos15@gmail.com>
2024-05-16 14:36:54 +0100

    .

---
+++ do_eos_delta.py
@@ -0,0 +1,42 @@
+import os, subprocess
+
+def do_eos_delta():
+    input_file = "delta_sorted.dat"
+    output_dir = "/beta-outputs"
+    temp_input_file = "delta.inp"
+    
+    #beta=r"C:/Users/lavin/OneDrive/Desktop/Lavinia/UT3/M1/Stage/delta-meson/original_data/beta-eq"
+    beta=r"C:/Users/lavin/OneDrive/Desktop/Lavinia/UT3/M1/Stage/delta-meson/original_data/beta-eq"
+    num_lines = 5
+
+    # Open the input file
+    with open(input_file, "r") as infile:
+        lines = infile.readlines()
+    
+    for i in range(1, num_lines + 1):
+        # Write the i-th line to the temporary input file
+        with open(temp_input_file, "w") as tempfile:
+            tempfile.write(lines[i - 1])
+        
+        print(i)
+        with open(temp_input_file, "r") as tempfile:
+            print(tempfile.read())
+        
+        with open(output_file, "w") as outfile:
+            outfile.write(lines[i - 1])
+        # output_file = f"{output_dir}/beta-eq-eos{i}`.dat"
+        # if os.path.exists(output_file):
+        #     os.remove(output_file)
+        
+        # Execute the external command
+        "./beta-eq"
+        
+        # Append the contents of fort.7 to the output file
+        with open("fort.7", "r") as fort_file, open(output_file, "a") as outfile:
+            outfile.write(fort_file.read())
+        
+        # Remove the temporary input file
+        if os.path.exists(temp_input_file):
+            os.remove(temp_input_file)
+
+do_eos_delta()
---
+++ sort_delta.py
@@ -0,0 +1,17 @@
+def sort_eos18():
+    # input_file = r"C:\Users\lavin\OneDrive\Desktop\Lavinia\UT3\M1\Stage\delta-meson\original_data\delta.dat"
+    # output_file = r"C:\Users\lavin\OneDrive\Desktop\Lavinia\UT3\M1\Stage\delta-meson\original_data\delta_sorted.dat"
+
+    input_file = "original_data/delta.dat"
+    output_file = "original_data/delta_sorted.dat"
+    
+    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+        for line in infile:
+            columns = line.split()
+            if len(columns) < 4:
+                continue
+            col2, col3, col4 = float(columns[1]), float(columns[2]), float(columns[3])
+            if 9.33 <= col2 <= 10.63 and 0.27 <= col3 <= 1.96 and 2.35 <= col4 <= 3.38:
+                outfile.write(line)
+
+sort_eos18()

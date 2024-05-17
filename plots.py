945a8f77631332422370a2a649eb490ec9f33561
lavinia.santos <laviniagtsantos15@gmail.com>
2024-05-17 14:58:11 +0100

    .

--- plots.py
+++ plots.py
@@ -5,13 +5,15 @@ import pandas as pd
 #### Plots
 
 input_dir = "tov-outputs"
-num_lines = 3
+num_lines = 5
 
 
 for i in range (1, num_lines + 1):
     input_file = f"{input_dir}/tov{i}.txt"
+    df = pd.DataFrame(columns = range(5))
     df = pd.read_csv(input_file, sep=" ", header=None, on_bad_lines='skip')
     df.columns = ["e0", "M", "R", "Mb","rc"]
+    print(len(df.columns))
     #print(df)
     #fig, ax = plt.subplots()
     plt.plot(df['R'],df['M'], label='file {i}')

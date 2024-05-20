d395cefe20f1ad753ed985e9e7a3b9ba39823edf
lavinia.santos <laviniagtsantos15@gmail.com>
2024-05-20 09:48:39 +0100

    .

--- original_data/full.py
+++ original_data/full.py
@@ -90,6 +90,38 @@ def do_tov_delta():
         with open(temp_input_file, "r") as tempfile:
             pass
         print(f"tov {i} done")
+def do_properties_delta():
+    input_file = "delta_sorted.dat"
+    output_dir = "properties-outputs"
+    temp_input_file = "delta.inp"
+    
+
+    # Open the input file
+    with open(input_file, "r") as infile:
+        lines = infile.readlines()
+    
+    for i in range(1, num_lines + 1):
+       with open(temp_input_file, "w") as tempfile:
+        pass
+       
+    
+        with open(temp_input_file, "w") as tempfile:
+            tempfile.write(lines[i].strip()+"\n")
+        
+        #print(i)
+        #with open(temp_input_file, "r") as tempfile:
+        #its not necessary to remove the old files because it overwrites them (w)
+        output_file = f"{output_dir}/prop{i}.txt"
+        # Execute the external command
+        result = subprocess.run("./properties", shell=True)
+            #result
+            # Write (overwrite!) the contents of fort.7 to the output file
+        with open("fort.60", "r") as fort_file, open(output_file, "w") as outfile:
+            outfile.write(fort_file.read())
+                
+        # Remove the temporary input file
+        if os.path.exists(temp_input_file):
+            os.remove(temp_input_file)
 
 def plot_mass_radius():
     input_dir = "tov-outputs"
@@ -149,10 +181,11 @@ def plot_energy_vs_density():
     plt.show()
     
 def main():
+    do_properties_delta()
     do_eos_delta()
     add_crust()
     do_tov_delta()
-    plot_energy_vs_density()
+    plot_mass_radius()
     #try to make subplots? Idk if it's necessary
 
 

d34d4cc90228b5e38c3a69ca7097ba8d0848dc23
lavinia.santos <laviniagtsantos15@gmail.com>
2024-05-22 16:49:08 +0100

    .

--- workdir/quartile.py
+++ workdir/quartile.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def quartile():
     #get mass values from the tov output file
     #for now, we use a dummy array
@@ -10,37 +11,51 @@ def quartile():
 
     mass=[]
     radius={}
-    with open(input_file, "r") as infile:
-        for line in infile:
-            columns = line.split()
-            #print(columns)
-            mass.append(columns[2])
-        #print(mass)
-            temp=[]
-            for i in range(1,num_curves+1):
-                print(f"i:{i}")
-                print(f"columns[0]:{columns[0]}")
-                if int(columns[0]) == i and int(columns[0]) not in radius.keys():
-                    # print(radius.keys())
-                    print(f"in if loop columns[0] (col):{columns[0]}")
+    # with open(input_file, "r") as infile:
+    #     for line in infile:
+    #         columns = line.split()
+    #         #print(columns)
+    #         mass.append(columns[2])
+    #     #print(mass)
+    #         temp=[]
+    #         for i in range(1,num_curves+1):
+    #             print(f"i:{i}")
+    #             print(f"columns[0]:{columns[0]}")
+    #             if int(columns[0]) == i and int(columns[0]) not in radius.keys():
+    #                 # print(radius.keys())
+    #                 print(f"in if loop columns[0] (col):{columns[0]}")
                     
-                    #temp = float(columns[3])
-                    # print(temp)
-                    radius[i] = []
-                    radius[i].append(float(columns[3]))
-                    #print(radius)
-                    print(f"radius[{i}] if:{radius[i]}")
+    #                 #temp = float(columns[3])
+    #                 # print(temp)
+    #                 radius[i] = []
+    #                 radius[i].append(float(columns[3]))
+    #                 #print(radius)
+    #                 print(f"radius[{i}] if:{radius[i]}")
 
-                elif int(columns[0]) == i and int(columns[0]) in radius.keys():
-                    print(f"in else loop columns[0] (col):{columns[0]}")
-                    #temp.append(columns[3])
-                    #print(temp)
-                    radius[i].append(float(columns[3]))
-                    print(f"radius[{i}] elif:{radius[i]}")
-        print(radius)
-        # print(f"radius:{radius.values()}")
+    #             elif int(columns[0]) == i and int(columns[0]) in radius.keys():
+    #                 print(f"in else loop columns[0] (col):{columns[0]}")
+    #                 #temp.append(columns[3])
+    #                 #print(temp)
+    #                 radius[i].append(float(columns[3]))
+    #                 print(f"radius[{i}] elif:{radius[i]}")
+    #     print(radius)
+    #     # print(f"radius:{radius.values()}")
 
 
+# make up some data
+
+# interpreting x value based on y value
+x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y=np.sin(x)
+y_val = 0.1
+x_interp = round(np.interp(y_val, y, x), 4)  # x_interp = np.interp(y_vals, y, x)
+
+print(f"x_interp:{x_interp}")
+
+# place a marker on point (x_interp, y_val)
+plt.plot(x, y, '-', color='k')
+plt.plot(x_interp, y_val, 'ro')
+plt.show()
                 
             
 

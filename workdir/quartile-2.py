48a38040d887d396eec570038d8692f7b1c226be
lavinia.santos <laviniagtsantos15@gmail.com>
2024-05-29 16:21:53 +0100

    .

--- workdir/quartile-2.py
+++ workdir/quartile-2.py
@@ -16,11 +16,16 @@ original_stdout = sys.stdout
 
 
 def plot_mr_curve():
+    M=[]
+    R=[]
+    q1=[]
+    q3=[]
+    Mass=[]
     # Load data from file
     N_points=200
     df = pd.read_csv('tov-data-ex.txt', sep='   ')
     df = df.rename(columns={df.columns[0]: 'label', df.columns[1]: 'energy density', df.columns[2]: 'mass', df.columns[3]: 'radius', df.columns[4]: 'baryonic masses', df.columns[5]: 'density'})
-
+    M_x=np.linspace(min(df.mass), max(df.mass), N_points)
     #print(df)
 
     labels = np.asarray(df['label'])
@@ -36,15 +41,39 @@ def plot_mr_curve():
         # print(f"M_Temp:{M_Temp}")
         R_Temp = np.asarray(df.radius[df.label == x])
         # print(f"R_Temp:{R_Temp}")
-        index=M_Temp.argmax()
-        #print(f"index:{index}")
-        # M_df=M_Temp[index:]
-        # R_df=R_Temp[index:]
+        # index=M_Temp.argmax()
+        # print(f"index:{index}")
+        # M_df=M_Temp[:index]
+        # R_df=R_Temp[:index]
         # print(f"M_df:{M_df}, R_df:{R_df}")
-
+        #if x !=1:
         tck = interp1d(M_Temp, R_Temp, bounds_error=False, kind='linear', fill_value=(np.nan, np.nan))
-        print(f"tck:{tck}")
 
+        for k in range(N_points):
+            R_Temp=tck(M_x[k])
+            M.append(M_x[k])
+            R.append(R_Temp)
+    eos=pd.DataFrame({'M':M, 'R':R})
+
+    #print(eos)
+    count=0
+    for mass in eos['M']:
+        print(f"{count} out of {len(eos['M'])} done")
+        count+=1
+        df_temp=eos.query(f'M=={mass}')
+
+        q1_temp,q3_temp=np.nanquantile(df_temp['R'], [0.25, 0.75])
+        q1.append(q1_temp)
+        q3.append(q3_temp)
+        Mass.append(mass)
+
+    # print(f"q1:{q1}")
+    # print(f"q3:{q3}")
+    # print(f"mass:{Mass}")
+
+    plt.plot(q1, Mass, 'r-')
+    plt.plot(q3, Mass, 'b-')
+    plt.show()
 
 
     

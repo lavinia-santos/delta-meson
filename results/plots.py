65d07426f420bbafee2f444b80b3bea8433f9187
lavinia.santos <laviniagtsantos15@gmail.com>
2024-06-05 17:23:17 +0100

    .

--- results/plots.py
+++ results/plots.py
@@ -8,13 +8,15 @@ input_dir = "tov-outputs"
 num_lines = 500
 #colors=['mediumpurple', 'darkviolet', 'deeppink', 'navy', 'deeppink', 'springgreen']
 
-eos_number=[8,18,20,21]
+eos_number=[20]
 
 
-# df = pd.read_csv(f'../../big-results/tov-data18.dat', sep='\s+', engine='python')
+# df = pd.read_csv(f'../../big-results/tov-data20.dat', sep='\s+', engine='python')
 # df = df.rename(columns={df.columns[0]: 'label', df.columns[1]: 'energy_density', df.columns[2]: 'mass', df.columns[3]: 'radius', df.columns[4]: 'baryonic_masses', df.columns[5]: 'density'})
-# df = df[df['radius'] < 13.5]
-# df=df[df['radius'] > 11.0]
+# df = df[df['radius'] < 14.5]
+# df=df[df['radius'] > 10.0]
+# df = df[df['mass'] < 2.30]
+
 # print(df)
 # #input_file = f"{input_dir}/tov{i}.txt"
 # #df = pd.read_csv(input_file, sep="   ", header=None, on_bad_lines='skip')
@@ -27,6 +29,8 @@ eos_number=[8,18,20,21]
 #     yi=temp_df['mass'].to_numpy()
 #     #needs to convert df to numpy because pandas and matplotlib doesn't get along with well
 #     plt.plot(xi, yi)
+# plt.xlabel('Radius')
+# plt.ylabel('Mass')
 # plt.legend()
 # plt.show()
 
@@ -39,7 +43,8 @@ for num in eos_number:
     quartiles = f"EOS{num}/output-quartile.txt"
     df = pd.read_csv(quartiles, sep=" ", header=None, on_bad_lines='skip')
     df = df.rename(columns={df.columns[0]: 'mass', df.columns[1]: 'q1', df.columns[2]: 'q3'})
-    print(df)
+    df = df[df['mass'] < 2.30]
+    # print(df)
     mass=df['mass'].to_numpy()
     q1=df['q1'].to_numpy()
     plt.plot(q1, mass, color='black', linestyle='dashed')
@@ -47,9 +52,9 @@ for num in eos_number:
     print(len(q3))
     plt.plot(q3, mass, color='black', linestyle='dashed')
     plt.legend()
+    #plt.xlim(11, 15)
     plt.title(f"EOS{num}")
-    #plt.show()
-    plt.savefig(f"quartile-plot{num}.png")
+    plt.show()
+    #plt.savefig(f"quartile-plot{num}.png")
     mass=[]
-    q1=[]
-    q3=[]
+ 
--- workdir/plots.py
+++ workdir/plots.py
@@ -12,7 +12,7 @@ for i in range (1, num_lines + 1):
 #for i in range(12,13):
 #if i!=12:
     #input_file = f"{input_dir}/tov{i}.txt"
-    input_file = f"output-quartile.txt"
+    input_file = f"../results/EOS20/output-quartile.txt"
     df = pd.read_csv(input_file, sep=" ", header=None, on_bad_lines='skip')
     df = df.rename(columns={df.columns[0]: 'mass', df.columns[1]: 'q1', df.columns[2]: 'q3'})
     #df = df.rename(columns={df.columns[0]: 'e0', df.columns[1]: 'M', df.columns[2]: 'R', df.columns[3]: 'Mb', df.columns[4]: 'rc'})
@@ -21,10 +21,12 @@ for i in range (1, num_lines + 1):
     xi=df['q1'].to_numpy()
     yi=df['mass'].to_numpy()
     #needs to convert df to numpy because pandas and matplotlib doesn't get along with well
-    plt.plot(xi, yi, color=colors[i%6])
+    plt.plot(xi, yi, color='black', linestyle='dashed')
+    #plt.plot(xi, yi, color=colors[i%6])
     xi=df['q3'].to_numpy()
     yi=df['mass'].to_numpy()
-    plt.plot(xi, yi, color=colors[i%6])
+    plt.plot(xi, yi, color='black', linestyle='dashed')
+    #plt.plot(xi, yi, color=colors[i%6])
 plt.legend()
 plt.show()
 

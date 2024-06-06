b29f61bc26c2955c12e80346c789d29ec1170afa
lavinia.santos <laviniagtsantos15@gmail.com>
2024-06-06 16:17:40 +0100

    .

--- results/plots.py
+++ results/plots.py
@@ -5,56 +5,57 @@ import pandas as pd
 #### Plots
 
 input_dir = "tov-outputs"
-num_lines = 500
-#colors=['mediumpurple', 'darkviolet', 'deeppink', 'navy', 'deeppink', 'springgreen']
+num_lines = 1000
+colors=['mediumpurple', 'darkviolet', 'deeppink', 'navy', 'deeppink', 'springgreen']
 
-eos_number=[20]
-
-
-# df = pd.read_csv(f'../../big-results/tov-data20.dat', sep='\s+', engine='python')
-# df = df.rename(columns={df.columns[0]: 'label', df.columns[1]: 'energy_density', df.columns[2]: 'mass', df.columns[3]: 'radius', df.columns[4]: 'baryonic_masses', df.columns[5]: 'density'})
-# df = df[df['radius'] < 14.5]
-# df=df[df['radius'] > 10.0]
-# df = df[df['mass'] < 2.30]
-
-# print(df)
-# #input_file = f"{input_dir}/tov{i}.txt"
-# #df = pd.read_csv(input_file, sep="   ", header=None, on_bad_lines='skip')
-# #df = df.rename(columns={df.columns[0]: 'e0', df.columns[1]: 'M', df.columns[2]: 'R', df.columns[3]: 'Mb', df.columns[4]: 'rc'})
-# labels = df['label'].unique()
-# print(f"labels:{labels}")
-# for label in labels:
-#     temp_df = df[df['label'] == label]
-#     xi=temp_df['radius'].to_numpy()
-#     yi=temp_df['mass'].to_numpy()
-#     #needs to convert df to numpy because pandas and matplotlib doesn't get along with well
-#     plt.plot(xi, yi)
-# plt.xlabel('Radius')
-# plt.ylabel('Mass')
-# plt.legend()
-# plt.show()
+eos_number=[21]
 
 
-for num in eos_number:
-    mass=[]
-    q1=[]
-    q3=[]
-    plt.clf()
-    quartiles = f"EOS{num}/output-quartile.txt"
-    df = pd.read_csv(quartiles, sep=" ", header=None, on_bad_lines='skip')
-    df = df.rename(columns={df.columns[0]: 'mass', df.columns[1]: 'q1', df.columns[2]: 'q3'})
-    df = df[df['mass'] < 2.30]
-    # print(df)
-    mass=df['mass'].to_numpy()
-    q1=df['q1'].to_numpy()
-    plt.plot(q1, mass, color='black', linestyle='dashed')
-    q3=df['q3'].to_numpy()
-    print(len(q3))
-    plt.plot(q3, mass, color='black', linestyle='dashed')
-    plt.legend()
-    #plt.xlim(11, 15)
-    plt.title(f"EOS{num}")
-    plt.show()
-    #plt.savefig(f"quartile-plot{num}.png")
-    mass=[]
+df = pd.read_csv(f'../../big-results/tov-data21.dat', sep='\s+', engine='python')
+df = df.rename(columns={df.columns[0]: 'label', df.columns[1]: 'energy_density', df.columns[2]: 'mass', df.columns[3]: 'radius', df.columns[4]: 'baryonic_masses', df.columns[5]: 'density'})
+df = df[df['radius'] < 14.5]
+df=df[df['radius'] > 10.0]
+df = df[df['mass'] > 0.5]
+
+print(df)
+#input_file = f"{input_dir}/tov{i}.txt"
+#df = pd.read_csv(input_file, sep="   ", header=None, on_bad_lines='skip')
+#df = df.rename(columns={df.columns[0]: 'e0', df.columns[1]: 'M', df.columns[2]: 'R', df.columns[3]: 'Mb', df.columns[4]: 'rc'})
+labels = df['label'].unique()
+print(f"labels:{labels}")
+for label in labels:
+        temp_df = df[df['label'] == label]
+        xi=temp_df['radius'].to_numpy()
+        yi=temp_df['mass'].to_numpy()
+        #needs to convert df to numpy because pandas and matplotlib doesn't get along with well
+        plt.plot(xi, yi, color=colors[label%6])
+plt.xlabel('Radius')
+plt.ylabel('Mass')
+plt.title('EOS21')
+plt.legend()
+plt.show()
+
+
+# for num in eos_number:
+#     mass=[]
+#     q1=[]
+#     q3=[]
+#     plt.clf()
+#     quartiles = f"EOS{num}/output-quartile.txt"
+#     df = pd.read_csv(quartiles, sep=" ", header=None, on_bad_lines='skip')
+#     df = df.rename(columns={df.columns[0]: 'mass', df.columns[1]: 'q1', df.columns[2]: 'q3'})
+#     df = df[df['mass'] >0.5]
+#     # print(df)
+#     mass=df['mass'].to_numpy()
+#     q1=df['q1'].to_numpy()
+#     plt.plot(q1, mass, color='black', linestyle='dashed')
+#     q3=df['q3'].to_numpy()
+#     print(len(q3))
+#     plt.plot(q3, mass, color='black', linestyle='dashed')
+#     plt.legend()
+#     #plt.xlim(11, 15)
+#     plt.title(f"EOS{num}")
+#     plt.show()
+#     plt.savefig(f"quartile-plot{num}.png")
+#     mass=[]
  
--- results/test_corner.py
+++ results/test_corner.py
@@ -4,37 +4,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-eos_number=[20,18,21,8]
+#eos_number=[20,21,8]
+eos_number=[21]
+#plot coupling for 21
+
 
 for i in eos_number:
-    props=f"EOS{i}/props.dat"
+    #props=f"EOS{i}/props.dat"
+    couplings="../../../../../../../../Downloads/para_eos21.csv"
     corner_plot="corner.dat"
-    with open( props, 'r') as r, open(corner_plot, 'w') as o: 
+    with open( couplings, 'r') as r, open(corner_plot, 'w') as o: 
         for line in r: 
             #strip() function 
             if line.strip(): 
                 o.write(line) 
     df=pd.read_csv(corner_plot, sep=",", header=None, skip_blank_lines=True, on_bad_lines='skip')
-    df=df.rename(columns={df.columns[9]: 'Esym', df.columns[10]: 'L', df.columns[11]: 'Ksym', df.columns[12]: 'Qsym'})
+    #df=df.rename(columns={df.columns[9]: 'Esym', df.columns[10]: 'L', df.columns[11]: 'Ksym', df.columns[12]: 'Qsym'})
+    df=df.rename(columns={df.columns[0]: 'label', df.columns[1]: 'gr', df.columns[2]: 'gdel', df.columns[3]: 'lambda'})
     # print(df)
-    df=df[df['L'] < 200]
-    df=df[df['L'] > 0]
-    df=df[df['Ksym'] > -200]
-    df=df[df['Ksym'] < 100]
-    df=df[df['Esym'] > 0]
-    df=df[df['Esym'] < 100]
-    df=df[df['Qsym'] > 1000]
-    df=df[df['Qsym'] < 4000]
+    # df=df[df['L'] < 200]
+    # df=df[df['L'] > 0]
+    # df=df[df['Ksym'] > -200]
+    # df=df[df['Ksym'] < 100]
+    # df=df[df['Esym'] > 0]
+    # df=df[df['Esym'] < 100]
+    # df=df[df['Qsym'] > 1000]
+    # df=df[df['Qsym'] < 4000]
     props1 = df.to_numpy()
     print(df)
-    good_columns=props1[:,9:13]
-    print(good_columns)
+    good_columns=props1[:,1:4]
+    # print(good_columns)
 
     # print(good_columns)
     
     
-    figure = corner.corner(good_columns, labels=["Esym", "L", "Ksym", "Qsym"], show_titles=True)
+    figure = corner.corner(good_columns, labels=["gr", "gdel", "lambda"], show_titles=True)
     # figure.show()
     # plt.show()
     figure.suptitle(f"EOS{i}", fontsize=16)
-    figure.savefig(f"corner-props{i}.png")
+    figure.savefig(f"corner-couplings{i}.png")
